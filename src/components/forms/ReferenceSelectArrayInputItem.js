a94d222f845adda3174561c4d6586ddc3a0af235
Facundo <facundo.ore@hotmail.com>
2022-11-08 10:51:40 -0300

    employee create and edit fixed

--- src/components/forms/ReferenceSelectArrayInputItem.js
+++ src/components/forms/ReferenceSelectArrayInputItem.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { ChipField, ReferenceArrayInput, SelectArrayInput} from "react-admin";
 
 const ReferenceSelectArrayInputItem = ({ referenceValues }) => {
-  const { source, reference, optionText } = referenceValues;
+  const { source, reference} = referenceValues;
   return (
     <>
       {
