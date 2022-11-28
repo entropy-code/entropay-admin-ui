333036e66669f85ca9ab953e7db4581fad489eb8
Maxi Ibañez <mibanez@workstride.com>
2022-11-28 10:21:43 -0300

    Remove unused field

--- src/assignments.js
+++ src/assignments.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Datagrid, DateField, EditButton, List, ReferenceField, TextField, DateInput, Edit, ReferenceInput, SelectInput, SimpleForm, NumberField, NumberInput,
-TextInput, Create, FunctionField, FormDataConsumer } from 'react-admin';
+TextInput, Create, FunctionField } from 'react-admin';
 
 export const AssignmentList = () => (
     <List>
@@ -52,21 +52,9 @@ export const AssignmentEdit = () => (
 export const AssignmentCreate = props => (
     <Create {...props}>
         <SimpleForm>
-            <FormDataConsumer>
-                {({ formData, ...rest }) =>
-                    formData.clientId && (
-                    <ReferenceInput 
-                        label="Projects"
-                        source="projectId"
-                        reference="projects"
-                        filter={{ clientId: formData.clientId }}
-                        {...rest}
-                    >
-                        <SelectInput optionText="name" />
-                    </ReferenceInput>
-                    )
-                }
-            </FormDataConsumer>
+            <ReferenceInput source="projectId" reference="projects">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
             <ReferenceInput source="employeeId" reference="employees">
                 <SelectInput optionText={record => `${record.firstName} ${record.lastName} (${record.internalId})`} />
             </ReferenceInput>
