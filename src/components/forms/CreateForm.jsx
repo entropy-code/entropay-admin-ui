6f9805b80dbdc1bfdebd2e9e91996eff9cbbbbd6
Facundo <facundo.ore@hotmail.com>
2023-01-13 10:44:54 -0300

    update in validation entity, end date can be null

--- src/components/forms/CreateForm.jsx
+++ src/components/forms/CreateForm.jsx
@@ -24,7 +24,7 @@ const CreateForm = ({ formData, title, resource }) => {
 
 const validateEntity = async (values) => {
   const errors = {};
-  if(values.endDate < values.startDate){
+  if(values.endDate < values.startDate && values.endDate != null){
     errors.endDate = "End Date can't be previous to Start Date";
   }
   return errors;
