6833fe7428892bc3aa67b804b066ade5a8ac0729
Facundo <facundo.ore@hotmail.com>
2023-03-30 10:22:14 -0300

    EN-72: Contract status based on Due date

--- src/components/forms/CreateForm.jsx
+++ src/components/forms/CreateForm.jsx
@@ -24,8 +24,8 @@ const CreateForm = ({ formData, title, resource }) => {
 
 const validateEntity = async (values) => {
   const errors = {};
-  if(values.endDate < values.startDate && values.endDate){
-    errors.endDate = "End Date can't be previous to Start Date";
+  if(values.endDate <= values.startDate && values.endDate){
+    errors.endDate = "End Date can't be previous or equals to Start Date";
   }
   return errors;
 }
