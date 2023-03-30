75e4d650110788cd3261aeac176cb6be53e5dd0d
Facundo <facundo.ore@hotmail.com>
2023-03-30 10:29:38 -0300

    additions

--- src/components/forms/EditForm.jsx
+++ src/components/forms/EditForm.jsx
@@ -8,7 +8,7 @@ const EditForm = ({ formData, title }) => {
 const validateEntity = async (values) => {
   const errors = {};
   if(values.endDate <= values.startDate && values.endDate){
-    errors.endDate = "End Date can't be previous to Start Date";
+    errors.endDate = "End Date can't be previous or equals to Start Date";
   }
   return errors;
 }
