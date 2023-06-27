31779a89fe45bc7ab2556b679b091424dce13a5d
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-06-27 11:34:56 -0300

    En-95:  set active field to assignment entity (#77)
    * EN-95:  Set active field to Assignment entity
    
    * EN-95:  changes to typescript
    
    * EN-95:  Add filter by active and type IQuickFilterProps
    
    * EN-95:  Add default filter true and and modularize QuickFilter to components/filters folder
    
    * EN-95:  Changes names quickFiltter and IQuickFilterModel
    
    * EN-95:  change quickFilter Models
    
    * EN-95:  change quickFilter Models
    
    * EN-95:  fix import
    
    * EN-95:  delete IQuickFilterModels

--- src/assignments.tsx
+++ src/assignments.tsx
@@ -15,7 +15,7 @@ import {
 import CreateForm from "./components/forms/CreateForm";
 import EditForm from "./components/forms/EditForm";
 import { IAssignment } from "./types";
-import QuickFilter from "./components/filters/quickFilter";
+import QuickFilter from "./components/filters/QuickFilter";
 
 function disabledCheck(source: string) {
   return source === "employeeProfile";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 export * from "./contracts";
 export * from "./asignment";
-export * from "./IQuickFilterModel";
+export * from "./quickFilterModel";
+
+
