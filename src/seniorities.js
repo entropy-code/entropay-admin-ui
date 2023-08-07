aa9d68a4521d442036750c93001015df7b9d70b5
FacuOrellana <70980835+FacuOrellana@users.noreply.github.com>
2023-08-07 12:57:54 -0300

    EN-105: Vacation creation job (#94)
    * add vacation day in seniotity form and list

--- src/seniorities.js
+++ src/seniorities.js
@@ -1,12 +1,21 @@
 import * as React from "react";
-import { Datagrid, List, TextField, EditButton } from "react-admin";
+import {
+  Datagrid,
+  List,
+  TextField,
+  EditButton,
+  NumberField,
+} from "react-admin";
 import CreateForm from "./components/forms/CreateForm";
 import EditForm from "./components/forms/EditForm";
 
 const formData = [
   {
     title: "Seniorities",
-    inputsList: [{ name: "name", type: "string", required: true }],
+    inputsList: [
+      { name: "name", type: "string", required: true },
+      { name: "vacationDays", type: "number", required: true },
+    ],
   },
 ];
 
@@ -14,6 +23,7 @@ export const SeniorityList = () => (
   <List>
     <Datagrid rowClick="edit">
       <TextField source="name" />
+      <NumberField source="vacationDays" />
       <EditButton />
     </Datagrid>
   </List>
