978aa8e0859e6e6f86d43dfc812b7d594db656b1
FedericoA15 <federicoalmeida15@gmail.com>
2023-09-28 11:31:31 -0300

    EN-109: new icon reporting

--- src/components/layout/CustomMenu.js
+++ src/components/layout/CustomMenu.js
@@ -1,45 +1,69 @@
-import { Sidenav, Nav } from 'rsuite';
-import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
-import GroupIcon from '@rsuite/icons/legacy/Group';
-import GearIcon from '@rsuite/icons/legacy/Gear';
-import PeopleGroup from '@rsuite/icons/legacy/PeopleGroup';
-import DocPassIcon from '@rsuite/icons/DocPass';
-import DetailIcon from '@rsuite/icons/Detail';
-import CalendarIcon from '@rsuite/icons/Calendar';
-import React from 'react';
-import 'rsuite/dist/rsuite.min.css';
+import { Sidenav, Nav } from "rsuite";
+import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
+import GroupIcon from "@rsuite/icons/legacy/Group";
+import GearIcon from "@rsuite/icons/legacy/Gear";
+import PeopleGroup from "@rsuite/icons/legacy/PeopleGroup";
+import DocPassIcon from "@rsuite/icons/DocPass";
+import DetailIcon from "@rsuite/icons/Detail";
+import CalendarIcon from "@rsuite/icons/Calendar";
+import ExportIcon from "@rsuite/icons/Export";
+import React from "react";
+import "rsuite/dist/rsuite.min.css";
 
 const icons = {
-    dashboard: <DashboardIcon />,
-    employees: <GroupIcon />,
-    contracts: <DocPassIcon />,
-    assignments: <DetailIcon />,
-    clients: <PeopleGroup />,
-    ptos: <CalendarIcon />,
-    settings: <GearIcon />
-  }
+  dashboard: <DashboardIcon />,
+  employees: <GroupIcon />,
+  contracts: <DocPassIcon />,
+  assignments: <DetailIcon />,
+  clients: <PeopleGroup />,
+  ptos: <CalendarIcon />,
+  settings: <GearIcon />,
+  reporting: <ExportIcon />,
+};
 
 export const CustomMenu = () => {
-  const config = JSON.parse(localStorage.getItem('config')) || {}
-    return (
+  const config = JSON.parse(localStorage.getItem("config")) || {};
+  return (
     <div style={{ width: 240 }}>
       <Sidenav>
         <Sidenav.Body>
           <Nav activeKey="1">
-          {config.menu && config.menu.map((item, index) => {
-              return (
-                item.href && !item.submenu 
-                ? <Nav.Item key={item.key} eventKey={item.key} icon={icons[item.icon]} href={item.href}>{item.name}</Nav.Item>
-                : <Nav.Menu key={item.key} eventKey={item.key} icon={icons[item.icon]} title={item.name}>
-                  {item.submenu && item.submenu.map((submenu, submenuIndex) => {
-                    return (
-                      <Nav.Item key={submenu.key} eventKey={submenu.key} href={submenu.href}>{submenu.name}</Nav.Item>
-                    )})}
+            {config.menu &&
+              config.menu.map((item, index) => {
+                return item.href && !item.submenu ? (
+                  <Nav.Item
+                    key={item.key}
+                    eventKey={item.key}
+                    icon={icons[item.icon]}
+                    href={item.href}
+                  >
+                    {item.name}
+                  </Nav.Item>
+                ) : (
+                  <Nav.Menu
+                    key={item.key}
+                    eventKey={item.key}
+                    icon={icons[item.icon]}
+                    title={item.name}
+                  >
+                    {item.submenu &&
+                      item.submenu.map((submenu, submenuIndex) => {
+                        return (
+                          <Nav.Item
+                            key={submenu.key}
+                            eventKey={submenu.key}
+                            href={submenu.href}
+                          >
+                            {submenu.name}
+                          </Nav.Item>
+                        );
+                      })}
                   </Nav.Menu>
-              )})}
+                );
+              })}
           </Nav>
         </Sidenav.Body>
       </Sidenav>
     </div>
-  )
+  );
 };
