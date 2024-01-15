413c9483b57d0a74afbbe8337258e1b03fdd3627
FedericoA15 <federicoalmeida15@gmail.com>
2024-01-15 17:19:01 -0300

    EN-145: Time off Reporting: total days per Employee

--- src/ptosReport.js
+++ src/ptosReport.js
@@ -7,9 +7,23 @@ import {
 } from "react-admin";
 import { exporter } from "./utils/exporter";
 
-const headers = ['Internal ID', 'First Name', 'Last Name', 'Client', 'Leave Reason', 'Total Days']
+const headers = [
+  "Internal ID",
+  "First Name",
+  "Last Name",
+  "Client",
+  "Leave Reason",
+  "Total Days",
+];
 
-const headersOrder = ['internalId', 'firstName', 'lastName', 'clientName', 'leaveTypeName', 'daysTaken']
+const headersOrder = [
+  "internalId",
+  "firstName",
+  "lastName",
+  "clientName",
+  "leaveTypeName",
+  "totalDays",
+];
 
 const reportFieldsList = [
   { name: "internalId", type: "number" },
@@ -17,7 +31,7 @@ const reportFieldsList = [
   { name: "lastName", type: "text" },
   { name: "clientName", type: "text" },
   { name: "leaveTypeName", type: "text" },
-  { name: "daysTaken", type: "number" },
+  { name: "totalDays", type: "number" },
 ];
 
 export const PtoReportList = () => {
@@ -25,17 +39,18 @@ export const PtoReportList = () => {
     <List
       resource="reports/ptos"
       exporter={exporter(reportFieldsList, "ptosReport", headers, headersOrder)}
-      actions={<> <ExportButton/> </>}
+      actions={
+        <>
+          <ExportButton />
+        </>
+      }
     >
-      <Datagrid
-        bulkActionButtons={false}
-      >
+      <Datagrid bulkActionButtons={false}>
         <TextField source="internalId" />
         <TextField source="firstName" />
-        <TextField source="lastName" /> 
+        <TextField source="lastName" />
         <TextField source="clientName" />
-        <TextField source="leaveTypeName" label="Leave Type" />
-        <NumberField source="daysTaken" />
+        <NumberField source="totalDays" />
       </Datagrid>
     </List>
   );
