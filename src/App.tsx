99a7166dfe8e382d6513a843401894778ef0c0e5
Maxi Ibanez <maxi.ibanez@velocityautomotive.com>
2024-11-20 11:05:36 -0300

    Use authProvider checkAuth

--- src/App.tsx
+++ src/App.tsx
@@ -23,19 +23,8 @@ const httpClient = (url: string, options: fetchUtils.Options = {}) => {
   return fetchUtils.fetchJson(url, options);
 };
 
-
-
 const fetchResources = async () => {
-  const token = localStorage.getItem("token");
-  const loginUrl =
-  config.config.api.userAuth +
-  "/auth/login?redirectUrl=" +
-  config.config.app.home;
-  if (token === null) {
-    window.location.href = loginUrl;
-    return Promise.resolve(null);
-  }
-
+  await authProvider.checkAuth();
   await authProvider.getPermissions();
   return resourceMap.map((r, index) => (
     <Resource
