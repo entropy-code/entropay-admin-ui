7bd28fc98ad5a0e7e4123d6d9673b339a76dce4f
Esteban Azaretzky <eazaretzky@gmail.com>
2025-02-17 19:11:02 -0300

    Add Billing Report with list view and export functionality (#176)
    Co-authored-by: eazaretzky <esteban@entropyteam.com>

---
+++ src/billingReport.tsx
@@ -0,0 +1,97 @@
+import {
+  Datagrid, DateInput,
+  ExportButton,
+  FilterButton,
+  List,
+  NumberField,
+  ReferenceField,
+  SearchInput,
+  TextField,
+} from "react-admin";
+import { exporter } from "./utils/exporter";
+import * as React from "react";
+import moment from "moment";
+
+
+const headersRename = [
+  "Internal ID",
+  "First Name",
+  "Last Name",
+  "Client Name",
+  "Project Name",
+  "Rate",
+  "Hours",
+  "Total",
+];
+
+const headers = [
+  "internalId",
+  "firstName",
+  "lastName",
+  "clientName",
+  "projectName",
+  "rate",
+  "hours",
+  "total",
+];
+
+const filterStyles = {
+  "& .MuiInputBase-root": {
+    height: "40px",
+  },
+};
+
+const parseISODate = (val?: string | null) => val && new Date(val).toISOString().slice(0, 10);
+
+const reportFilters = [
+  <DateInput source="startDate"
+             alwaysOn
+             label="Start Date"
+             sx={filterStyles}
+             inputProps={{ inputFormat: "YYYY-MM-DD" }}
+             parse={parseISODate}
+  />,
+  <DateInput source="endDate"
+             alwaysOn
+             label="End Date"
+             sx={filterStyles}
+             inputProps={{ inputFormat: "YYYY-MM-DD" }}
+             parse={parseISODate}
+  />,
+];
+
+export const BillingReportList = () => {
+  return (
+    <List
+      resource="reports/billing"
+      exporter={exporter("billingReport", headers, headersRename)}
+      actions={
+        <>
+          <FilterButton />
+          <ExportButton />
+        </>
+      }
+      filters={reportFilters}
+      disableSyncWithLocation
+    >
+      <Datagrid bulkActionButtons={false}>
+        <ReferenceField
+          source="employeeId"
+          reference="employees"
+          link="show"
+          label="Internal ID"
+          sortBy="internalId"
+        >
+          <TextField source="internalId" />
+        </ReferenceField>
+        <TextField source="firstName" />
+        <TextField source="lastName" />
+        <TextField source="clientName" />
+        <TextField source="projectName" />
+        <NumberField source="rate" />
+        <NumberField source="hours" />
+        <NumberField source="total" />
+      </Datagrid>
+    </List>
+  );
+};
